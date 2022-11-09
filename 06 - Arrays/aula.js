7d067d4d37188b7f0c97be467fdda28a68c74433
José Alberto <97975327+Joseal19@users.noreply.github.com>
2022-11-09 19:52:46 -0300

    Arrays

---
+++ 06 - Arrays/aula.js
@@ -0,0 +1,18 @@
+const alunos = [
+  {
+    nome: 'José',
+    idade: '20'
+  },
+  {
+    nome: 'Pable',
+    idade: '25'
+  },
+];
+
+// push adiciona alguma coisa dentro do array
+alunos.push(  {
+  nome: 'João',
+  idade: '35'
+})
+
+console.log(alunos[2]);
