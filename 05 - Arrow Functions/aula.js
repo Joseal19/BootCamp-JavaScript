e34384b600746c214ecd03747ac4b8bb338298d8
José Alberto <97975327+Joseal19@users.noreply.github.com>
2022-11-09 19:47:20 -0300

    Arrow Function

---
+++ 05 - Arrow Functions/aula.js
@@ -0,0 +1,21 @@
+function subtrair(num1, num2){
+  return num1 - num2;
+}
+
+const subtrair = (num1, num2) =>{
+  num1 - num2;
+};
+
+function somar(num1, num2){
+  return num1 + num2;
+}
+
+const somar = (num1, num2) =>{
+  num1 + num2;
+};
+
+
+
+
+const resultado  = somar(10,5);
+console.log("🚀 ~ file: aula.js ~ line 11 ~ resultado", resultado)
